e3394ec03c6b55277fb4cf452aaf756ad9ff5782
keeeeeey <sseioul@naver.com>
2024-02-01 11:26:44 +0900

    [feat]: controller advice 추가

---
+++ src/main/java/com/example/flow/common/exception/ExceptionEnum.java
@@ -0,0 +1,19 @@
+package com.example.flow.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ExceptionEnum {
+    RUNTIME_EXCEPTION(HttpStatus.BAD_REQUEST, "E0001", "내부 문제로 다음번에 다시 시도해주세요."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E0002", "내부 문제로 다음번에 다시 시도해주세요."),
+    API_NOT_EXIST_EXCEPTION(HttpStatus.NOT_FOUND, "E0003", "존재하지 않는 API 입니다."),
+    API_METHOD_NOT_ALLOWED_EXCEPTION(HttpStatus.METHOD_NOT_ALLOWED, "E0004", "지원하지 않는 Method 입니다."),
+    API_PARAMETER_EXCEPTION(HttpStatus.BAD_REQUEST, "E0005", "파라미터 타입과 값을 확인하세요.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
